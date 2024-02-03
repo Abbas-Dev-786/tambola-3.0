8bceb42844eea721527a911b655a3fe36aa5119f
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-02-03 19:16:55 +0530

    added dummy ticket data

--- frontend/src/components/Ticket/Ticket.jsx
+++ frontend/src/components/Ticket/Ticket.jsx
@@ -2,29 +2,59 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Ticket.css";
 
+const data = [
+  "earning from rewards",
+  null,
+  null,
+  "Version-control",
+  "Java or Kotlin",
+  null,
+  "Digital currency",
+  "JetBrains",
+  null,
+  "Decentralized application.",
+  "void main()",
+  null,
+  "ID",
+  "Oculus, Vive",
+  null,
+  null,
+  "Portability",
+  null,
+  "Fields",
+  "Prim's Algorithm",
+  "Text",
+  null,
+  null,
+  "Unit of data",
+  null,
+  "HTML",
+  null,
+];
+
 function Ticket() {
   const [getTicket, setGetTicket] = useState([]); //get values for ticket
   const user_id = JSON.parse(localStorage.getItem("user"))?.id; //get user id
 
   //get values inside ticket once render to route
   useEffect(() => {
-    async function fetchTicket() {
-      //get array of answers from db
-      //   const URL = "https://tambola-backend.vercel.app/ticket";
-      const URL = "http://127.0.0.1:5000/ticket";
-
-      try {
-        const res = await axios.post(URL, {
-          data: { id: user_id },
-        });
-        // store answers in getTicket state
-        setGetTicket(res.data["answers"]);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    //function calling
-    fetchTicket();
+    setGetTicket(data);
+    // async function fetchTicket() {
+    //   //get array of answers from db
+    //   //   const URL = "https://tambola-backend.vercel.app/ticket";
+    //   const URL = "http://127.0.0.1:5000/ticket";
+    //   try {
+    //     const res = await axios.post(URL, {
+    //       data: { id: user_id },
+    //     });
+    //     // store answers in getTicket state
+    //     setGetTicket(res.data["answers"]);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    // //function calling
+    // fetchTicket();
   }, [user_id]);
 
   //change the background of block of ticket when clicked
