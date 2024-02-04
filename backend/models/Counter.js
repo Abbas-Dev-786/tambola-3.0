828e368f4112eaf0852f70bc66c29eb31f34fd4e
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-02-04 14:29:23 +0530

    fixed ticket issue

--- backend/models/Counter.js
+++ backend/models/Counter.js
@@ -1,33 +1,34 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 //To auto assign an increasing id to tickets generated
 
-const counterSchema = new Schema(
-  {
-  _id: {type: String, required: true}, //model on which it has to be applied i.e."ticket"
-  seq: { type: Number, default: 0 } //current count
-  }
-);
+const counterSchema = new Schema({
+  _id: { type: String, required: true }, //model on which it has to be applied i.e."ticket"
+  seq: { type: Number, default: 0 }, //current count
+});
 
-counterSchema.index({ _id: 1, seq: 1 }, { unique: true }) //starting index
+counterSchema.index({ _id: 1, seq: 1 }, { unique: true }); //starting index
 
-const counterModel = mongoose.model('counter', counterSchema);
+const counterModel = mongoose.model("counter", counterSchema);
 
 //auto increasing function for counter model
 
-const autoIncrementModelID = function (modelName, doc, next) {
-  counterModel.findByIdAndUpdate(        
-    modelName,
-    { $inc: { seq: 1 } },
-    { new: true, upsert: true }, 
-    function(error, counter) {    
-      if(error) return next(error);
+const autoIncrementModelID = async function (modelName, doc, next) {
+  try {
+    const counter = await counterModel
+      .findByIdAndUpdate(
+        modelName,
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+      )
+      .exec();
 
-      doc.id = counter.seq;
-      next();
-    }
-  );                    
-}
+    doc.id = counter.seq;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = autoIncrementModelID;
